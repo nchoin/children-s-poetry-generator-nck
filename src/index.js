4259e0e3fbc42da391f370592c801492fd385b03
nchoin <139597297+nchoin@users.noreply.github.com>
2024-01-31 16:46:27 -0500

    created the function to get the poem from AI and display on the screen.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-function generatePoem(event){
-    event.preventDefault();
-    console.log("get poem");
-    let poemElement = document.querySelector("#ai-poem");
-
+function displayPoem(response){
+    // respsonse.data.answer
     new Typewriter("#ai-poem", {
-      strings: "Nicole Choiniere-Kroeker",
+      strings: response.data.answer,
       autoStart: true,
       delay: 40,
       cursor: null,
     });
 }
 
+
+
+function generatePoem(event) {
+  event.preventDefault();
+  console.log("get poem");
+  let poemTopic = document.querySelector("#poem-topic");
+  let apiKey = "6bccfefa354f0f4do4245dc0a56fata0";
+  let prompt = `User instructions: generate a poem about ${poemTopic.value}`;
+  let context =
+    "You are an AI master poet. You create poems for people ages five to eleven years old. Sometimes your poems are funny. Most of the time your poems rhyme. Your mission is to generate a poem no longer than 8 lines formatted in basic HTML. Make sure to follow the user's instructions. Do not include a title. Sign the poem with 'by SheCodes AI' inside a <strong> element at the end of the poem.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("getting the API") 
+  console.log(`${prompt}`);
+  console.log(`${context}`)
+// call the api and send response to new function called displayPoem
+  axios.get(apiUrl).then(displayPoem);
+
+}
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
